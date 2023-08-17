a688301b0e15aa67c30fbe7ab90d4c0d3c4214f2
Dibyajyoti Dey <dibyajyoti49@outlook.com>
2023-08-17 09:48:22 +0530

    git ignore

--- 3.3.c
+++ 3.3.c
@@ -2,8 +2,8 @@
 // Created by dibyajyotidey on 10/8/23.
 //
 //wap to calculate the area of a rectangle and the perimenter of it by providing length and breadth of it.
-
 #include <stdio.h>
+
 void main(){
     int len, bre;
     printf("Enter the length and breadth of the rectangle: \n");
--- 6.1.c
+++ 6.1.c
@@ -1,8 +1,18 @@
 //
 // Created by dibyajyotidey on 17/8/23.
-//
+// wap to find the greater number between 3 numbers.
+
 #include <stdio.h>
 
 void main(){
-
+    int x,y,z;
+    printf("Enter 3 numbers:\n");
+    scanf("%d %d %d", &x, &y, &z);
+    if(x>y){
+        if(x>z){
+            printf("%d is the greatest number", x);
+        }else{
+            printf("%d is the greatest number", z);
+        }
+    }
 }
