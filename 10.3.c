9871026b438c508ad183e3e13a07a560bef6245c
Dibyajyoti Dey <dibyajyoti49@outlook.com>
2023-08-31 10:28:52 +0530

    day-10

--- 10.3.c
+++ 10.3.c
@@ -2,3 +2,20 @@
 // Created by dibyajyotidey on 31/8/23.
 //
 //wap to compare two strings
+#include "stdio.h"
+#include "string.h"
+
+void main(){
+    char s[100], s1[100];
+    printf("Enter string 1: ");
+    scanf("%[^\n]%*c", s);
+    printf("Enter string 2: ");
+    scanf("%[^\n]%*c", s1);
+    if(strcmp(s, s1)>0){
+        printf("String 1 is greater than String 2.\n");
+    } else if(strcmp(s, s1)==0){
+        printf("String 1 is equal to String 2.\n");
+    } else{
+        printf("String 1 is smaller than String 2.\n");
+    }
+}
